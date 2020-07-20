ee385de947a97d0cddda94efa19043741fad14ff
Adam Leszczynski <aleszczynski@bersler.com>
2020-07-20 07:09:04 +0200

    update copyright information

--- scripts/grants.sql
+++ scripts/grants.sql
@@ -1,4 +1,21 @@
---Example file with list of required grants for OpenLogReplicator
+/* List of grants required for user in Oracle database
+   Copyright (C) 2018-2020 Adam Leszczynski (aleszczynski@bersler.com)
+
+This file is part of OpenLogReplicator.
+
+OpenLogReplicator is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+OpenLogReplicator is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenLogReplicator; see the file LICENSE;  If not see
+<http://www.gnu.org/licenses/>.  */
 
 GRANT SELECT ON SYS.CCOL$ TO <USER>;
 GRANT SELECT ON SYS.CDEF$ TO <USER>;
